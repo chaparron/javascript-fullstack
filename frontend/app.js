0e56c43a2c7c53e77c7b0b4e7e51c51b6e70598e
chaparron <chaparron@github.com>
2019-12-25 01:39:01 +0100

    ya mostramos por pantalla los libros a tiempo real, y al añadirlos también se ven automáticamente

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,10 @@
 import './styles/app.css';
-import BookService from './services/BookService';
+import UI from './UI';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const ui = new UI();
+	ui.renderBooks();
+});
 
 document.getElementById('book-form')
 	.addEventListener('submit', e => {
@@ -12,10 +17,10 @@ document.getElementById('book-form')
     formData.append('image', image[0]);
     formData.append('title', title);
     formData.append('author', author);
-    formData.append('isbn', isbn);
-
-	const bookService = new BookService()
-	bookService.postBook(formData)
+	formData.append('isbn', isbn);
+	
+	const ui = new UI;
+	ui.addANewBook(formData);
 
 	e.preventDefault();
 	});
